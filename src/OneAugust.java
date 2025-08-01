e98f321627cd999755798ead0c629293dd16fac9
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-01 20:54:19 +0530

    Code of August

--- src/OneAugust.java
+++ src/OneAugust.java
@@ -2,12 +2,12 @@ public class OneAugust {
     public static String interpret(String command) {
         StringBuilder sb = new StringBuilder();
         int i = 0;
-        while(i < command.length()) {
-            if(command.charAt(i) == 'G') {
+        while (i < command.length()) {
+            if (command.charAt(i) == 'G') {
                 sb.append('G');
                 i++;
             } else {
-                if(command.charAt(i + 1) == ')') {
+                if (command.charAt(i + 1) == ')') {
                     sb.append('o');
                     i += 2;
                 } else {
@@ -22,12 +22,28 @@ public class OneAugust {
 
     public static String restoreString(String s, int[] indices) {
         char[] res = new char[s.length()];
-        for(int i = 0; i<indices.length; i++){
+        for (int i = 0; i < indices.length; i++) {
             res[indices[i]] = s.charAt(i);
         }
         String result = String.valueOf(res);
         return result;
     }
+
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                sb.append((char) (ch + 32));
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+        // return s.toLowerCase();
+    }
+
     public static void main(String[] args) {
 
     }
