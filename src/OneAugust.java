daa821839e81e4aba7c7d4a25d3d58403f69d8cb
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-01 20:53:42 +0530

    Code of August

--- src/OneAugust.java
+++ src/OneAugust.java
@@ -1,4 +1,33 @@
 public class OneAugust {
+    public static String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i < command.length()) {
+            if(command.charAt(i) == 'G') {
+                sb.append('G');
+                i++;
+            } else {
+                if(command.charAt(i + 1) == ')') {
+                    sb.append('o');
+                    i += 2;
+                } else {
+                    sb.append("al");
+                    i += 4;
+                }
+            }
+        }
+        return sb.toString();
+        // return command.replace("()", "o").replace("(al)", "al");
+    }
+
+    public static String restoreString(String s, int[] indices) {
+        char[] res = new char[s.length()];
+        for(int i = 0; i<indices.length; i++){
+            res[indices[i]] = s.charAt(i);
+        }
+        String result = String.valueOf(res);
+        return result;
+    }
     public static void main(String[] args) {
 
     }
