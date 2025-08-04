2871df781438005551a306373abf594cee6d4c35
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-04 16:14:55 +0530

    Code of August

--- src/FourAugust.java
+++ src/FourAugust.java
@@ -98,9 +98,26 @@ public class FourAugust {
         return result;
     }
 
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5, 6};
+
+        int[] arr1 = {0, 1, 2, 3, 4};
+
+        int[] index = {0, 1, 2, 2, 1};
         System.out.println(Arrays.toString(runningSum(arr)));
 
         System.out.println(Arrays.toString(shuffleArray(arr, 3)));
@@ -114,6 +131,8 @@ public class FourAugust {
         System.out.println(Arrays.toString(smallerNumbersThanCurrent(arr)));
 
         System.out.println(Arrays.toString(smallerNumbersThanCurrentOptimal(arr)));
+
+        System.out.println(Arrays.toString(createTargetArray(arr1, index)));
 
     }
 }
