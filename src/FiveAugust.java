1f53baab40896668df75f1257d839551ffb6a736
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-05 16:03:08 +0530

    Code of August

--- src/FiveAugust.java
+++ src/FiveAugust.java
@@ -87,7 +87,7 @@ public class FiveAugust {
 
         int k = 1;
         for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[i-1]) {
+            if (nums[i] != nums[i - 1]) {
                 nums[k] = nums[i];
                 k++;
             }
@@ -95,17 +95,17 @@ public class FiveAugust {
         return k;
     }
 
-    public static int removeDuplicatesOptimal(int[] nums){
+    public static int removeDuplicatesOptimal(int[] nums) {
         int n = nums.length;
 
         int i = 0;
-        for(int j = 1; j< n; j++){
-            if(nums[i] != nums[j]){
+        for (int j = 1; j < n; j++) {
+            if (nums[i] != nums[j]) {
                 i++;
                 nums[i] = nums[j];
             }
         }
-        return i+1;
+        return i + 1;
     }
 
     public static void main(String[] args) {
