f17e5736a5868b6d8e4ebc5ebb93b7fd27e33829
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-05 12:35:32 +0530

    Code of August

--- src/FiveAugust.java
+++ src/FiveAugust.java
@@ -1,24 +1,40 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FiveAugust {
-    public static List<Integer> addToArrayForm(int[] num, int k){
+    public static List<Integer> addToArrayForm(int[] num, int k) {
         LinkedList<Integer> list = new LinkedList<>();
-        int i = num.length-1;
+        int i = num.length - 1;
 
-        while(i>=0 || k > 0){
-            if(i>=0){
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
                 k = k + num[i];
                 i--;
             }
-            list.addFirst(k%10);
+            list.addFirst(k % 10);
             k = k / 10;
         }
         return list;
     }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,0,0};
+        int[] arr = {1, 2, 0, 0};
         int k = 34;
         System.out.println(addToArrayForm(arr, k));
+
+        System.out.println(Arrays.toString(twoSum(arr, 5)));
     }
 }
