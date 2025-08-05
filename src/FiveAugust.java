a1c98c30e99cea3ca705d01b7d2b26c01b4ab5de
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-05 15:23:12 +0530

    Code of August

--- src/FiveAugust.java
+++ src/FiveAugust.java
@@ -44,6 +44,29 @@ public class FiveAugust {
         return null;
     }
 
+    public static int maxSubArray(int[] nums) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        int maxElement = Integer.MIN_VALUE;
+
+
+        for (int i = 0; i < nums.length; i++) {
+            cs = cs + nums[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+
+            if (nums[i] > maxElement) {
+                maxElement = nums[i];
+            }
+        }
+        if (ms == 0 && maxElement < 0) {
+            return maxElement;
+        }
+        return ms;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 0, 0};
         int k = 34;
