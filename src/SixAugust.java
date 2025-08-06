bfc64e7effba301b8f500fb528fbecf5af307fb0
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-06 12:01:12 +0530

    Code of August

--- src/SixAugust.java
+++ src/SixAugust.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SixAugust {
 
     public static int pick = 42;
@@ -30,10 +32,31 @@ public class SixAugust {
         return -1;
     }
 
-    public static void main(String[] args) {
+    public static int[] twoSumII(int[] num, int target){
+        int start = 0;
+        int end = num.length-1;
 
+        while(start<=end){
+            int total = num[start] + num[end];
+
+            if(total == target){
+                return new int[] {start+1, end+1};
+            }else if(total>target){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,4,2,5,6,1,8,5};
         int n = 100;
         System.out.println("Guessed number: " + guessNumber(n));
+
+        System.out.println(Arrays.toString(twoSumII(nums, 15)));
+
 
     }
 }
