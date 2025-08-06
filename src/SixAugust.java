80a430411e4319d56ae76024e18e3adf9d8daec3
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-06 12:11:00 +0530

    Code of August

--- src/SixAugust.java
+++ src/SixAugust.java
@@ -50,6 +50,28 @@ public class SixAugust {
         return new int[]{-1, -1};
     }
 
+    public static boolean isPerfectSquare(int num) {
+        long start = 0;
+        long end = num/2;
+
+        if(num < 2){
+            return true;
+        }
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            long sqrt = mid*mid;
+
+            if(sqrt == num){
+                return true;
+            }else if(sqrt < num){
+                start = mid+1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 4, 2, 5, 6, 1, 8, 5};
         int n = 100;
@@ -57,6 +79,7 @@ public class SixAugust {
 
         System.out.println(Arrays.toString(twoSumII(nums, 15)));
 
+        System.out.println(isPerfectSquare(45));
 
     }
 }
