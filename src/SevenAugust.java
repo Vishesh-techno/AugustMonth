72f42729b142c50cae1944b9a6ca6cf54d39b418
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-07 17:41:17 +0530

    Code of August

--- src/SevenAugust.java
+++ src/SevenAugust.java
@@ -119,13 +119,38 @@ public class SevenAugust {
         return -1;
     }
 
+    public static int singleNonDuplicateOptimal(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        } else if (nums[0] != nums[1]) {
+            return nums[0];
+        } else if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
+        int start = 1;
+        int end = nums.length - 2;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            } else if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 6, 8, 9};
 
         int[] nums1 = {5, 7, 7, 8, 8, 8, 10};
 
-        int[] nums2 = {1,1,2,3,3,4,4,8,8};
+        int[] nums2 = {1, 1, 2, 3, 3, 4, 4, 8, 8};
 
         System.out.println(searchInsert(nums, 9));
 
@@ -136,6 +161,8 @@ public class SevenAugust {
         System.out.println(search(nums, 5));
 
         System.out.println(Arrays.toString(searchRange(nums1, 8)));
+
+        System.out.println(singleNonDuplicates(nums2));
 
         System.out.println(singleNonDuplicates(nums2));
     }
