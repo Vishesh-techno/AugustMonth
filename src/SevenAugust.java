516dd2519fcf309148fe64004bd072219ab003ce
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-07 13:11:23 +0530

    Code of August

--- src/SevenAugust.java
+++ src/SevenAugust.java
@@ -29,14 +29,28 @@ public class SevenAugust {
         return ans;
     }
 
-//    public static int peakIndexMountainArrayOptimal(int[] nums){
-//
-//    }
+    public static int peakIndexMountainArrayOptimal(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<end){
+            int mid = start +(end-start)/2;
+
+            if (nums[mid] < nums[mid+1]){
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
     public static void main(String[] args) {
         int[] nums = {1,3,5,6,8,9};
 
         System.out.println(searchInsert(nums, 9));
 
         System.out.println(peakIndexMountainArray(nums));
+
+        System.out.println(peakIndexMountainArrayOptimal(nums));
     }
 }
