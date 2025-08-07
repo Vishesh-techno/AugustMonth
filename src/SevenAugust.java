f9061b2fb3292a8d88f71db09d507d6a1ba445fd
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-07 13:11:34 +0530

    Code of August

--- src/SevenAugust.java
+++ src/SevenAugust.java
@@ -1,27 +1,27 @@
 public class SevenAugust {
-    public static int searchInsert(int[] nums, int target){
+    public static int searchInsert(int[] nums, int target) {
         int start = 0;
-        int end = nums.length-1;
+        int end = nums.length - 1;
 
-        while(start < end){
-            int mid = start +(end-start)/2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[mid] < target){
+            } else if (nums[mid] < target) {
                 start = mid + 1;
-            }else{
+            } else {
                 end = mid;
             }
         }
         return start;
     }
 
-    public static int peakIndexMountainArray(int[] nums){
+    public static int peakIndexMountainArray(int[] nums) {
         int max = Integer.MIN_VALUE;
         int ans = 0;
-        for(int i=0; i<nums.length; i++){
-            if(max < nums[i]){
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
                 max = nums[i];
                 ans = i;
             }
@@ -29,23 +29,24 @@ public class SevenAugust {
         return ans;
     }
 
-    public static int peakIndexMountainArrayOptimal(int[] nums){
+    public static int peakIndexMountainArrayOptimal(int[] nums) {
         int start = 0;
-        int end = nums.length-1;
+        int end = nums.length - 1;
 
-        while(start<end){
-            int mid = start +(end-start)/2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
-            if (nums[mid] < nums[mid+1]){
+            if (nums[mid] < nums[mid + 1]) {
                 start = mid + 1;
-            }else {
+            } else {
                 end = mid - 1;
             }
         }
         return start;
     }
+
     public static void main(String[] args) {
-        int[] nums = {1,3,5,6,8,9};
+        int[] nums = {1, 3, 5, 6, 8, 9};
 
         System.out.println(searchInsert(nums, 9));
 
