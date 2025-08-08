35ccb33aec2e4f50a970c2960715b97eae121951
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-08 18:19:26 +0530

    Code of August

--- src/EightAugust.java
+++ src/EightAugust.java
@@ -16,12 +16,12 @@ public class EightAugust {
     public static String interpret(String command) {
         StringBuilder sb = new StringBuilder();
         int i = 0;
-        while(i < command.length()) {
-            if(command.charAt(i) == 'G') {
+        while (i < command.length()) {
+            if (command.charAt(i) == 'G') {
                 sb.append('G');
                 i++;
             } else {
-                if(command.charAt(i + 1) == ')') {
+                if (command.charAt(i + 1) == ')') {
                     sb.append('o');
                     i += 2;
                 } else {
@@ -36,7 +36,7 @@ public class EightAugust {
 
     public static String restoreString(String s, int[] indices) {
         char[] res = new char[s.length()];
-        for(int i = 0; i<indices.length; i++){
+        for (int i = 0; i < indices.length; i++) {
             res[indices[i]] = s.charAt(i);
         }
         String result = String.valueOf(res);
@@ -46,11 +46,11 @@ public class EightAugust {
     public static String toLowerCase(String s) {
         StringBuilder sb = new StringBuilder();
         int n = s.length();
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if(ch >= 'A' && ch <='Z'){
-                sb.append((char)(ch+32));
-            }else{
+            if (ch >= 'A' && ch <= 'Z') {
+                sb.append((char) (ch + 32));
+            } else {
                 sb.append(ch);
             }
         }
@@ -65,16 +65,15 @@ public class EightAugust {
 
         String str1 = "codeleet";
 
-        int[] arr = {4,5,6,7,0,2,1,3};
+        int[] arr = {4, 5, 6, 7, 0, 2, 1, 3};
 
         System.out.println(defangIPaddr(str));
 
         System.out.println(interpret(goal));
 
-       restoreString(str1, arr);
+        restoreString(str1, arr);
 
-       toLowerCase(str1);
-
+        toLowerCase(str1);
 
 
     }
