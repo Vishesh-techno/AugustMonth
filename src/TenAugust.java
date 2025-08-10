84383d127b9d2d0bbc6674342af54804ce61d557
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-10 23:27:14 +0530

    Code of August

--- src/TenAugust.java
+++ src/TenAugust.java
@@ -4,48 +4,83 @@ import java.util.Map;
 class ListNode {
     int val;
     ListNode next;
-    ListNode(int val) { this.val = val; }
+    ListNode(int val) {
+        this.val = val;
+    }
 }
 
-
 public class TenAugust {
+
+    // ---------- Two Sum ----------
     public static int[] twoSum(int[] nums, int target) {
-        // int n = nums.length;
-        // for(int i=0; i<n-1; i++){
-        //     for(int j=i+1; j<n; j++){
-        //         if(nums[i] + nums[j] == target){
-        //             return new int[]{i, j};
-        //         }
-        //     }
-        // }
-        // return new int[] {0,0};
-
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
+        for(int i = 0; i < nums.length; i++) {
             int lookupnumber = target - nums[i];
-
-            if(map.containsKey(lookupnumber)){
-                return new int[] {i, map.get(lookupnumber)};
+            if(map.containsKey(lookupnumber)) {
+                return new int[] {map.get(lookupnumber), i}; // Correct order
             }
-
             map.put(nums[i], i);
         }
-        return null;
+        throw new IllegalArgumentException("No two sum solution");
     }
+
+    // ---------- Detect Cycle in Linked List ----------
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // ---------- Reverse Linked List ----------
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         ListNode next;
-        while(curr != null){
+        while(curr != null) {
             next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
-        head = prev;
-        return head;
+        return prev; // new head
     }
-    public static void main(String[] args) {
 
+    // ---------- Main Method for Testing ----------
+    public static void main(String[] args) {
+        TenAugust obj = new TenAugust();
+
+        // --- Test Two Sum ---
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Two Sum: " + result[0] + " " + result[1]);
+
+        // --- Test Reverse List ---
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        head = obj.reverseList(head);
+        System.out.print("Reversed Linked List: ");
+        while(head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+
+        // --- Test Has Cycle ---
+        ListNode cycleHead = new ListNode(1);
+        cycleHead.next = new ListNode(2);
+        cycleHead.next.next = new ListNode(3);
+        cycleHead.next.next.next = cycleHead.next; // create cycle
+
+        System.out.println("Has Cycle: " + obj.hasCycle(cycleHead));
     }
 }
