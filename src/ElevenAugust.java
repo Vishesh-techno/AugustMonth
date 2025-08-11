ee84957c30dbd3567393d4f9c72a1a9383e2fa73
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-11 22:11:45 +0530

    Code of August

--- src/ElevenAugust.java
+++ src/ElevenAugust.java
@@ -20,24 +20,24 @@ public class ElevenAugust {
         int cs = 0;
         int ms = Integer.MIN_VALUE;
         int maxElement = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             cs = cs + nums[i];
-            if(cs < 0){
+            if (cs < 0) {
                 cs = 0;
             }
-            ms = Math.max(cs,ms);
-            if(nums[i] > maxElement){
+            ms = Math.max(cs, ms);
+            if (nums[i] > maxElement) {
                 maxElement = nums[i];
             }
         }
-        if(ms == 0 && maxElement < 0){
+        if (ms == 0 && maxElement < 0) {
             return maxElement;
         }
         return ms;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3,4,5,1,2};
+        int[] nums = {3, 4, 5, 1, 2};
         System.out.println(findMin(nums));
         System.out.println(maxSubArray(nums));
     }
