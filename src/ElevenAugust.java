1f9c976869d55c6b63cda6b77551c536fb86fdc2
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-11 22:10:25 +0530

    Code of August

---
+++ src/ElevenAugust.java
@@ -0,0 +1,22 @@
+public class ElevenAugust {
+    public static int findMin(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+            if (nums[start] > nums[mid] && nums[mid] < nums[end]) {
+                return nums[mid];
+            }
+        }
+        return nums[start];
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(findMin(nums));
+    }
+}
