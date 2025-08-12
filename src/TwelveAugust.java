e0750bb2aeaaafacfde0dba57d8678c6dd7afc8e
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-12 16:37:05 +0530

    Code of August

--- src/TwelveAugust.java
+++ src/TwelveAugust.java
@@ -35,8 +35,9 @@ public class TwelveAugust {
         }
         return res;
     }
+
     public static void main(String[] args) {
-        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = {-1, 0, 1, 2, -1, -4};
 
         System.out.println(threeSum(nums));
 
