3e55899075fd233b60c52fb0f7f23db9ab885e61
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-12 16:44:32 +0530

    Code of August

--- src/TwelveAugust.java
+++ src/TwelveAugust.java
@@ -4,6 +4,52 @@ import java.util.List;
 
 public class TwelveAugust {
 
+    // Node class for Linked List
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    // Method to remove N-th node from end of list
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int sz = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            sz++;
+            temp = temp.next;
+        }
+
+        if (head == null || n <= 0) {
+            return head;
+        }
+
+        if (n == sz) { // Remove first node
+            return head.next;
+        }
+
+        if (n > sz) { // If n is larger than list size
+            return head;
+        }
+
+        int i = 0;
+        int j = sz - n;
+        ListNode prev = head;
+        while (i < j - 1) {
+            prev = prev.next;
+            i++;
+        }
+        if (prev.next != null) {
+            prev.next = prev.next.next;
+        }
+        return head;
+    }
+
+    // Method to find all triplets whose sum is zero
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums); // Step 1: sort the array
@@ -37,9 +83,25 @@ public class TwelveAugust {
     }
 
     public static void main(String[] args) {
+
         int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println("Three Sum Result: " + threeSum(nums));
 
-        System.out.println(threeSum(nums));
 
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        TwelveAugust obj = new TwelveAugust();
+        head = obj.removeNthFromEnd(head, 2); // 2nd node from end
+
+        System.out.print("Linked List after removal: ");
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
     }
 }
