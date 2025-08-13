3105671584f5b1a5d6d263de5f06845f7937f033
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-13 18:09:49 +0530

    Code Of August

--- src/ThirteenAugust.java
+++ src/ThirteenAugust.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class ThirteenAugust {
     public static boolean isAnagram(String s, String t) {
         if (t.length() != s.length()) {
@@ -19,10 +21,32 @@ public class ThirteenAugust {
         return true;
     }
 
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
 
     public static void main(String[] args) {
         String str = "anagram";
         String str1 = "nagaram";
         System.out.println(isAnagram(str, str1));
+
+        String str2 = "([)]";
+        System.out.println(isValid(str2));
     }
 }
