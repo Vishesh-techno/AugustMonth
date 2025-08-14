a2b4d2def74b7440ac334f28c08a8119ef576f5b
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-14 15:51:25 +0530

    Code of August

--- src/FourteenAugust.java
+++ src/FourteenAugust.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class FourteenAugust {
     public static void rotate(int[] nums, int k) {
-        k = k % nums.length;
+        k = k % nums.length; // In case k > length
         revNum(nums, 0, nums.length - 1);
         revNum(nums, 0, k - 1);
         revNum(nums, k, nums.length - 1);
@@ -15,7 +17,38 @@ public class FourteenAugust {
             end--;
         }
     }
-    public static void main(String[] args) {
 
+    public static int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        Arrays.fill(output, 1); // Fill with 1 initially
+
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        // Test for rotate method
+        int[] nums1 = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        System.out.println("Original array: " + Arrays.toString(nums1));
+        rotate(nums1, k);
+        System.out.println("Array after rotating by " + k + ": " + Arrays.toString(nums1));
+
+        // Test for productExceptSelf method
+        int[] nums2 = {1, 2, 3, 4};
+        int[] product = productExceptSelf(nums2);
+        System.out.println("Input array for productExceptSelf: " + Arrays.toString(nums2));
+        System.out.println("Product except self: " + Arrays.toString(product));
     }
 }
