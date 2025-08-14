b46b43f9d43b40f5e7ac626b30a7b36ae34d5299
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-14 15:48:47 +0530

    Code of August

--- src/FourteenAugust.java
+++ src/FourteenAugust.java
@@ -1,5 +1,20 @@
 public class FourteenAugust {
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        revNum(nums, 0, nums.length - 1);
+        revNum(nums, 0, k - 1);
+        revNum(nums, k, nums.length - 1);
+    }
 
+    public static void revNum(int[] nums, int start, int end) {
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
     public static void main(String[] args) {
 
     }
