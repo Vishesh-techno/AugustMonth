04c841a20d7b0b4e962215fdc203ef58d2083681
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-17 16:34:00 +0530

    Code of August

--- src/SeventeenAugust.java
+++ src/SeventeenAugust.java
@@ -27,6 +27,46 @@ public class SeventeenAugust {
 
         return res;
     }
+
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int rowStart = 0, rowEnd = n - 1;
+        int colStart = 0, colEnd = n - 1;
+        int num = 1;
+
+        while (num <= n * n) {
+            // Traverse right
+            for (int i = colStart; i <= colEnd; i++) {
+                matrix[rowStart][i] = num++;
+            }
+            rowStart++;
+
+            // Traverse down
+            for (int i = rowStart; i <= rowEnd; i++) {
+                matrix[i][colEnd] = num++;
+            }
+            colEnd--;
+
+            // Traverse left
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    matrix[rowEnd][i] = num++;
+                }
+            }
+            rowEnd--;
+
+            // Traverse up
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    matrix[i][colStart] = num++;
+                }
+            }
+            colStart++;
+        }
+
+        return matrix;
+    }
+
     public static void main(String[] args) {
 
     }
