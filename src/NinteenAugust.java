e023355da139f9132236feab234336ac27b39bee
Vishesh-techno <sonivishesh099@gmail.com>
2025-08-19 18:01:53 +0530

    Code of August

---
+++ src/NinteenAugust.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NinteenAugust {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        revNum(nums, 0, nums.length - 1);
+        revNum(nums, 0, k - 1);
+        revNum(nums, k, nums.length - 1);
+    }
+
+    public void revNum(int[] nums, int start, int end) {
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int x = 0;
+        int y = 0;
+        int dx = 1;
+        int dy = 0;
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < rows * cols; i++) {
+            res.add(matrix[y][x]);
+            matrix[y][x] = -101; // the range of numbers in matrix is from -100 to 100
+
+            if (!(0 <= x + dx && x + dx < cols && 0 <= y + dy && y + dy < rows) || matrix[y + dy][x + dx] == -101) {
+                int temp = dx;
+                dx = -dy;
+                dy = temp;
+            }
+
+            x += dx;
+            y += dy;
+        }
+
+        return res;
+    }
+
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int rowStart = 0, rowEnd = n - 1;
+        int colStart = 0, colEnd = n - 1;
+        int num = 1;
+
+        while (num <= n * n) {
+            // Traverse right
+            for (int i = colStart; i <= colEnd; i++) {
+                matrix[rowStart][i] = num++;
+            }
+            rowStart++;
+
+            // Traverse down
+            for (int i = rowStart; i <= rowEnd; i++) {
+                matrix[i][colEnd] = num++;
+            }
+            colEnd--;
+
+            // Traverse left
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    matrix[rowEnd][i] = num++;
+                }
+            }
+            rowEnd--;
+
+            // Traverse up
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    matrix[i][colStart] = num++;
+                }
+            }
+            colStart++;
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        ThreeAugust obj = new ThreeAugust();
+
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        obj.rotate(nums, k);
+        System.out.print("Rotated Array: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        List<Integer> spiral = obj.spiralOrder(matrix);
+        System.out.println("Spiral Order: " + spiral);
+
+
+        int n = 4;
+        int[][] generatedMatrix = obj.generateMatrix(n);
+        System.out.println("Generated Spiral Matrix:");
+        for (int[] row : generatedMatrix) {
+            for (int val : row) {
+                System.out.print(val + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
