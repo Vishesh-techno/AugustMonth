88caf1b246d3810fe1d05ed6e3d2a5e6dbc349c9
Vishesh soni <sonivishesh099@gmail.com>
2025-08-20 21:22:28 +0530

    Code Of August

---
+++ src/TwentyAugust.java
@@ -0,0 +1,23 @@
+public int[] twoSum(int[] nums, int target) {
+        // int n = nums.length;
+        // for(int i=0; i<n-1; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }
+        // return new int[] {0,0};
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int lookupnumber = target - nums[i];
+
+            if(map.containsKey(lookupnumber)){
+                return new int[] {i, map.get(lookupnumber)};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
