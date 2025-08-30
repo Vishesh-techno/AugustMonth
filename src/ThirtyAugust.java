6b7fcfb5f8834bbad6c8bffe1803a5aaeff835e0
Vishesh soni <sonivishesh099@gmail.com>
2025-08-30 19:14:43 +0530

    Create ThirtyAugust.java

---
+++ src/ThirtyAugust.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        // if (n <= 0) return false; // powers of 3 are positive
+        // while (n % 3 == 0) {
+        //     n /= 3; // keep dividing by 3
+        // }
+        // return n == 1;
+        if(n <= 0){
+            return false;
+        }
+        while(n%3 == 0){
+            n /= 3;
+        }
+        if(n == 1){
+            return true;
+        }
+        return false; 
+    }
+}
